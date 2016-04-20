4344efbbde30dbf83172d4c28184966a33db451b
Byoungyoung Lee <lifeasageek@gmail.com>
2016-04-20 10:49:48 -0400

    use xdg-open as a default open-with

--- mu4e/mu4e-view.el
+++ mu4e/mu4e-view.el
@@ -1194,8 +1194,8 @@ If CMD is nil, ask user for it."
   (let* ((att (mu4e~view-get-attach msg attachnum))
 	  (cmd (or cmd
 		 (read-string
-		   (mu4e-format "Shell command to open it with: ")
-		   nil 'mu4e~view-open-with-hist)))
+		   (mu4e-format "Shell command to open with (default: xdg-open): ")
+		   nil 'mu4e~view-open-with-hist "xdg-open")))
 	  (index (plist-get att :index)))
     (mu4e~view-temp-action
       (mu4e-message-field msg :docid) index "open-with" cmd)))
